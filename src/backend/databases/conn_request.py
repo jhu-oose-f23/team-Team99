93999222a9c59cf06980a8cf04732129a6280297
Ishan Hemmige <ishan.hemmige@gmail.com>
2023-10-15 17:28:41 -0400

    added endpoints to create and accept connection requests

--- src/backend/databases/conn_request.py
+++ src/backend/databases/conn_request.py
@@ -33,3 +33,21 @@ def check_requests(source,dest):
 def delete_request(source, dest):
   data = supabase.table("Connection_Requests").delete().eq("source", source).eq("dest", dest).execute()
   return data.data[0] if data.data else "Request does not exist"
+
+def get_requests(dest):
+  data = supabase.table("Connection_Requests").select("*").eq("dest", dest).execute().data
+  return [data["source"] for data in data]
+
+def accept_connection(source, dest):
+  if not check_requests(source, dest):
+    return "Request does not exist"
+  supabase.table("Connection_Requests").delete().eq("source", source).eq("dest", dest).execute()
+  new_conn = {
+    "user1": source,
+    "user2": dest
+  }
+  try:
+    supabase.table("Connections").insert(new_conn).execute().data[0]
+  except:
+    return "Connection already exists"
+  return supabase.table("Connections").select("*").eq("user1", source).eq("user2", dest).execute().data[0]
--- src/backend/resources/conn_request.py
+++ src/backend/resources/conn_request.py
@@ -1,13 +1,13 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from flask import make_response
-from databases.conn_request import create_request, delete_request
-from schemas import ConnectionSchema, ConnectionRequestSchema
+from databases.conn_request import create_request, delete_request, get_requests, accept_connection
+from schemas import ConnectionRequestSchema, ConnectionSchema
 
 blp = Blueprint("connection requests", __name__, description="Operations on connection requests")
 
 @blp.route("/connection/request")
-class ConnectionRequestSchema(MethodView):
+class ConnectionRequest(MethodView):
   @blp.arguments(ConnectionRequestSchema)
   @blp.response(201, ConnectionRequestSchema)
   def post(self, connection):
@@ -27,4 +27,25 @@ class ConnectionRequestSchema(MethodView):
     response = make_response(result)
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
+  
+  @blp.arguments(ConnectionRequestSchema)
+  @blp.response(200, ConnectionSchema)
+  def put(self, connection):
+    result = accept_connection(connection["source"], connection["dest"])
+    if type(result) == str:
+      abort(404, message=result)
+    response = make_response(result)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+  
 
+@blp.route("/connection/request/<string:dest>")
+class ConnectionRequest(MethodView):
+  @blp.response(200, ConnectionRequestSchema)
+  def get(self, dest):
+    result = get_requests(dest)
+    if type(result) == str:
+      abort(404, message=result)
+    response = make_response(result)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
