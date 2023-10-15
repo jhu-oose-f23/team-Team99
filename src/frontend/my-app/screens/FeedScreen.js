a7202de9a29cb6365c4d39e6404969584d1c653e
shihd1 <46411183+shihd1@users.noreply.github.com>
2023-10-15 10:01:58 -0400

    Create Nav Bar

---
+++ src/frontend/my-app/screens/FeedScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const FeedScreen = () => {
+  return <View style={{ flex: 1, padding: 10 }}></View>;
+};
+
+export default FeedScreen;
