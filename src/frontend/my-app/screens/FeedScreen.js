827b35ab81127739b0685c5cf5798cc00d1bf849
Sambhav Chordia <sambhavchordia03@gmail.com>
2023-10-15 13:45:24 -0400

    added search functionality

--- src/frontend/my-app/screens/FeedScreen.js
+++ src/frontend/my-app/screens/FeedScreen.js
@@ -1,5 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, ScrollView, Image, TouchableOpacity, TextInput } from "react-native";
+
+const users = [
+  {
+    id: 4,
+    name: "Lucas Brown",
+    image: require("../assets/icon.png"),
+  },
+  {
+    id: 5,
+    name: "Emily Clark",
+    image: require("../assets/icon.png"),
+  },
+  // ... Add more users as needed
+];
 
 const recommendations = [
   {
@@ -22,6 +36,8 @@ const recommendations = [
 
 const FeedScreen = () => {
   const [connectionRequests, setConnectionRequests] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isSearchActive, setIsSearchActive] = useState(false);
 
   const sendConnectionRequest = (profileId) => {
       // Simulate a network request to send a connection request
@@ -31,92 +47,107 @@ const FeedScreen = () => {
     }, 1000);
   };
 
+  const filteredUsers = users.filter(user => 
+    user.name.toLowerCase().includes(searchQuery.toLowerCase())
+  ); 
   return (
     <View style={{ flex: 1, padding: 10 }}>
-      {/* Post */}
-      <View
-        style={{ backgroundColor: "#ffffff", padding: 10, marginBottom: 10 }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <Image
-            source={require("../assets/icon.png")}
-            style={{
-              width: 50,
-              height: 50,
-              borderRadius: 30,
-            }}
-          />
-          <View style={{ padding: 5 }}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>Post Title</Text>
-            <Text>Post Author</Text>
-          </View>
-        </View>
-        <Image
-          source={require("../assets/favicon.png")}
-          style={{
-            width: 300,
-            height: 300,
-            marginBottom: 10,
-          }}
-        />
-        <Text>Post content goes here...</Text>
-      </View>
+      {/* Search Bar */}
+      <TextInput
+        style={{
+          borderColor: "#ccc",
+          borderWidth: 1,
+          borderRadius: 5,
+          padding: 10,
+          marginBottom: 10,
+        }}
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+        placeholder="Search..."
+        onFocus={() => setIsSearchActive(true)}
+        onBlur={() => setIsSearchActive(false)}
+      />
 
-      {/* Recommendations */}
-      <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>
-        Recommendations
-      </Text>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {recommendations.map((profile) => (
+      {/* Displaying content based on whether the search bar is active */}
+      {isSearchActive ? (
+        // Search Results for Users
+        <ScrollView>
+          {filteredUsers.map((user) => (
+            <View key={user.id} style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 15 }}>
+              <Image
+                source={user.image}
+                style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                  marginRight: 10,
+                }}
+              />
+              <Text>{user.name}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      ) : (
+        <>
+          {/* Your Post component */}
           <View
-            key={profile.id}
-            style={{
-              backgroundColor: "#ffffff",
-              padding: 10,
-              marginRight: 10,
-              alignItems: "center",
-              height: 150,
-              width: 150,
-            }}
+            style={{ backgroundColor: "#ffffff", padding: 10, marginBottom: 10 }}
           >
-            <Image
-              source={profile.image}
-              style={{
-                width: 60,
-                height: 60,
-                borderRadius: 30,
-                marginBottom: 10,
-              }}
-            />
-            <Text style={{ fontSize: 16, fontWeight: "bold" }}>
-              {profile.name}
-            </Text>
-            <TouchableOpacity
-              style={{
-                backgroundColor: connectionRequests.includes(profile.id)
-                  ? "green"
-                  : "#007bff",
-                padding: 5,
-                borderRadius: 5,
-                marginTop: 10,
-              }}
-              onPress={() => sendConnectionRequest(profile.id)}
-              disabled={connectionRequests.includes(profile.id)}
-            >
-              <Text style={{ color: "#fff" }}>
-                {connectionRequests.includes(profile.id)
-                  ? "Request Sent"
-                  : "Connect"}
-              </Text>
-            </TouchableOpacity>
+            {/* ... Rest of your Post code */}
           </View>
-        ))}
-      </ScrollView>
+
+          {/* Recommendations */}
+          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>
+            Recommendations
+          </Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {recommendations.map((profile) => (
+              <View
+                key={profile.id}
+                style={{
+                  backgroundColor: "#ffffff",
+                  padding: 10,
+                  marginRight: 10,
+                  alignItems: "center",
+                  height: 150,
+                  width: 150,
+                }}
+              >
+                <Image
+                  source={profile.image}
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 30,
+                    marginBottom: 10,
+                  }}
+                />
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  {profile.name}
+                </Text>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: connectionRequests.includes(profile.id)
+                      ? "green"
+                      : "#007bff",
+                    padding: 5,
+                    borderRadius: 5,
+                    marginTop: 10,
+                  }}
+                  onPress={() => sendConnectionRequest(profile.id)}
+                  disabled={connectionRequests.includes(profile.id)}
+                >
+                  <Text style={{ color: "#fff" }}>
+                    {connectionRequests.includes(profile.id)
+                      ? "Request Sent"
+                      : "Connect"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </ScrollView>
+        </>
+      )}
     </View>
   );
 };
--- src/frontend/my-app/screens/SearchDisplay.js
+++ src/frontend/my-app/screens/SearchDisplay.js
@@ -1,1 +1,24 @@
-test;
+// SearchBar.js
+import React from 'react';
+import { TextInput } from 'react-native';
+
+const SearchBar = ({ value, onChange, onFocus, onBlur }) => {
+  return (
+    <TextInput
+      style={{
+        borderColor: "#ccc",
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+      }}
+      value={value}
+      onChangeText={onChange}
+      placeholder="Search..."
+      onFocus={onFocus}
+      onBlur={onBlur}
+    />
+  );
+};
+
+export default SearchBar;
