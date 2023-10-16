bc22427df833ba3f72f331a61c438fa4f99d3724
K02D <kirondeb02@gmail.com>
2023-10-15 20:37:32 -0400

    add navigation to settings and connections pages

--- src/frontend/my-app/BottomTabNavigator.js
+++ src/frontend/my-app/BottomTabNavigator.js
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FeedScreen from "./screens/FeedScreen";
 import CreateScreen from "./CreateWorkout";
 import ProfileScreen from "./screens/Profile";
+import SettingsPage from "./screens/SettingsPage";
+import ConnectionsPage from "./screens/connectionsPage";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,6 +19,14 @@ const BottomTabNavigator = ({ username }) => {
       <Tab.Screen
         name="Profile"
         children={() => <ProfileScreen username={username} />}
+      />
+      <Tab.Screen
+        name="Settings"
+        children={() => <SettingsPage username={username} />}
+      />
+      <Tab.Screen
+        name="Connections"
+        children={() => <ConnectionsPage username={username} />}
       />
     </Tab.Navigator>
   );
