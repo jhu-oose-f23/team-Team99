3bcb258d7b40834f32c23efe6fd841170bee1874
Peter Goh <pgoh4@jhu.edu>
2023-11-01 11:07:40 -0400

    Cleaned up comments

--- src/frontend/my-app/screens/Login.js
+++ src/frontend/my-app/screens/Login.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from "react";
 import { View, Text, TextInput, Button, Alert, StyleSheet } from "react-native";
 import UserContext from "../UserContext";
-// Define the Supabase API URL for login
 const loginApiUrl = "https://gymconnectbackend.onrender.com/user/login";
 
 const Login = ({ navigation, route }) => {
@@ -35,8 +34,6 @@ const Login = ({ navigation, route }) => {
           },
         });
 
-        // TODO: Store the username in a secure location
-        // Here, we are simply alerting the username
         Alert.alert(
           "Login Successful",
           `You are now logged in as ${userData.username}`
@@ -74,7 +71,7 @@ const Login = ({ navigation, route }) => {
       <Button
         title="Login"
         onPress={handleLogin}
-        disabled={isLoading} // Disable the button while loading
+        disabled={isLoading} 
       />
     </View>
   );
