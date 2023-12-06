0b4fd65e97fe79a4cb9bad968ad3a664ca2f210b
K02D <kirondeb02@gmail.com>
2023-12-06 12:00:10 -0500

    center dropdown text and use scrollview

--- src/frontend/screens/Signup.js
+++ src/frontend/screens/Signup.js
@@ -7,6 +7,7 @@ import {
   Text,
   TouchableWithoutFeedback,
   Keyboard,
+  ScrollView,
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import DropDownPicker from "react-native-dropdown-picker";
@@ -47,10 +48,27 @@ const Signup = ({ navigation, route }) => {
   const [preferredTimeDropdownOpen, setPreferredTimeDropdownOpen] =
     useState(false);
 
+  const [level, setLevel] = useState("");
+  const [levelOptions, setLevelOptions] = useState([
+    { label: "Beginner", value: "Beginner" },
+    { label: "Intermediate", value: "Intermediate" },
+    { label: "Advanced", value: "Advanced" },
+  ]);
+  const [levelDropdownOpen, setLevelDropdownOpen] = useState(false);
+
+  const [goal, setGoal] = useState([]);
+  const [goalOptions, setGoalOptions] = useState([
+    { label: "Meet people", value: "Meet people" },
+    { label: "Build muscle", value: "Build muscle" },
+    { label: "Get stronger", value: "Get stronger" },
+    { label: "Lose weight", value: "Lose weight" },
+    { label: "Build endurance", value: "Build endurance" },
+  ]);
+  const [goalDropdownOpen, setGoalDropdownOpen] = useState(false);
+
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
   const [goals, setGoals] = useState([]);
-  const [level, setLevel] = useState("");
 
   const { setUserHasSignedUp } = useContext(UserContext);
 
@@ -75,127 +93,205 @@ const Signup = ({ navigation, route }) => {
   };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <Text style={styles.label}>First Name</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="First Name"
-          onChangeText={setFirstName}
-        />
-
-        <Text style={styles.label}>Last Name</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Last Name"
-          onChangeText={setLastName}
-        />
-
-        <View style={{ zIndex: 1000, alignItems: "center" }}>
-          <Text style={{ marginRight: "50%" }}>Frequency</Text>
-          <DropDownPicker
-            open={frequencyDropdownOpen}
-            listMode="SCROLLVIEW"
-            value={frequency}
-            items={frequencyOptions}
-            setOpen={setFrequencyDropdownOpen}
-            setValue={(v) => setFrequency(v())}
-            placeholder="Frequency"
-            style={styles.dropdown}
-            placeholderStyle={{
-              color: "#C7C7CD",
-              marginBottom: 8,
-            }}
-            dropDownContainerStyle={{ ...styles.dropdown, zIndex: 1000 }}
-            containerStyle={{
-              maxHeight: 2000,
-            }}
+    <ScrollView>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text style={styles.label}>First Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="First Name"
+            onChangeText={setFirstName}
           />
-        </View>
 
-        <View style={{ zIndex: 100, alignItems: "center" }}>
-          <Text style={{ marginRight: "55%" }}>Gender</Text>
-          <DropDownPicker
-            open={genderDropdownOpen}
-            listMode="SCROLLVIEW"
-            value={gender}
-            items={genderOptions}
-            setOpen={setGenderDropdownOpen}
-            setValue={(v) => setGender(v())}
-            placeholder="Gender"
-            style={styles.dropdown}
-            placeholderStyle={{
-              color: "#C7C7CD",
-              marginBottom: 8,
-            }}
-            dropDownContainerStyle={styles.dropdown}
-            containerStyle={{
-              maxHeight: 2000,
-            }}
+          <Text style={styles.label}>Last Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Last Name"
+            onChangeText={setLastName}
           />
-        </View>
-        <View style={{ zIndex: 50, alignItems: "center" }}>
-          <Text style={{ marginRight: "45%" }}>Preferred Time</Text>
-          <DropDownPicker
-            open={preferredTimeDropdownOpen}
-            listMode="SCROLLVIEW"
-            value={preferredTime}
-            items={preferredTimeOptions}
-            setOpen={setPreferredTimeDropdownOpen}
-            setValue={(v) => setPreferredTime(v())}
-            placeholder="Preferred Time"
-            style={styles.dropdown}
-            placeholderStyle={{
-              color: "#C7C7CD",
-              marginBottom: 8,
-            }}
-            dropDownContainerStyle={styles.dropdown}
-            containerStyle={{
-              maxHeight: 2000,
-            }}
+
+          <View style={{ zIndex: 1000, alignItems: "center" }}>
+            <Text style={{ marginRight: "50%" }}>Frequency</Text>
+            <DropDownPicker
+              open={frequencyDropdownOpen}
+              listMode="SCROLLVIEW"
+              value={frequency}
+              items={frequencyOptions}
+              setOpen={setFrequencyDropdownOpen}
+              setValue={(v) => setFrequency(v())}
+              placeholder="Frequency"
+              style={styles.dropdown}
+              placeholderStyle={{
+                color: "#C7C7CD",
+                marginBottom: 8,
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+              dropDownContainerStyle={{ ...styles.dropdown, zIndex: 1000 }}
+              containerStyle={{
+                maxHeight: 2000,
+              }}
+              textStyle={{
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+            />
+          </View>
+
+          <View style={{ zIndex: 100, alignItems: "center" }}>
+            <Text style={{ marginRight: "55%" }}>Gender</Text>
+            <DropDownPicker
+              open={genderDropdownOpen}
+              listMode="SCROLLVIEW"
+              value={gender}
+              items={genderOptions}
+              setOpen={setGenderDropdownOpen}
+              setValue={(v) => setGender(v())}
+              placeholder="Gender"
+              style={styles.dropdown}
+              placeholderStyle={{
+                color: "#C7C7CD",
+                marginBottom: 8,
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+              dropDownContainerStyle={styles.dropdown}
+              containerStyle={{
+                maxHeight: 2000,
+              }}
+              textStyle={{
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+            />
+          </View>
+          <View style={{ zIndex: 50, alignItems: "center" }}>
+            <Text style={{ marginRight: "45%" }}>Preferred Time</Text>
+            <DropDownPicker
+              open={preferredTimeDropdownOpen}
+              listMode="SCROLLVIEW"
+              value={preferredTime}
+              items={preferredTimeOptions}
+              setOpen={setPreferredTimeDropdownOpen}
+              setValue={(v) => setPreferredTime(v())}
+              placeholder="Preferred Time"
+              style={styles.dropdown}
+              placeholderStyle={{
+                color: "#C7C7CD",
+                marginBottom: 8,
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+              dropDownContainerStyle={styles.dropdown}
+              containerStyle={{
+                maxHeight: 2000,
+              }}
+              textStyle={{
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+            />
+          </View>
+          <View style={{ zIndex: 40, alignItems: "center" }}>
+            <Text style={{ marginRight: "60%" }}>Level</Text>
+            <DropDownPicker
+              open={levelDropdownOpen}
+              listMode="SCROLLVIEW"
+              value={level}
+              items={levelOptions}
+              setOpen={setLevelDropdownOpen}
+              setValue={(v) => setLevel(v())}
+              placeholder="Level"
+              style={styles.dropdown}
+              placeholderStyle={{
+                color: "#C7C7CD",
+                marginBottom: 8,
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+              dropDownContainerStyle={styles.dropdown}
+              containerStyle={{
+                maxHeight: 2000,
+              }}
+              textStyle={{
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+            />
+          </View>
+          <View style={{ zIndex: 20, alignItems: "center" }}>
+            <Text style={{ marginRight: "60%" }}>Goal</Text>
+            <DropDownPicker
+              open={goalDropdownOpen}
+              multiple={true}
+              multipleText={`${goal.length} goal${
+                goal.length > 1 ? "s" : ""
+              } selected`}
+              listMode="SCROLLVIEW"
+              value={goal}
+              items={goalOptions}
+              setOpen={setGoalDropdownOpen}
+              setValue={setGoal}
+              placeholder="Goal"
+              style={styles.dropdown}
+              placeholderStyle={{
+                color: "#C7C7CD",
+                marginBottom: 8,
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+              textStyle={{
+                textAlign: "center",
+                marginLeft: 30,
+              }}
+              dropDownContainerStyle={styles.dropdown}
+              containerStyle={{
+                maxHeight: 2000,
+              }}
+            />
+          </View>
+
+          <Text style={styles.label}>Height (in)</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            onChangeText={setHeight}
+            value={height}
+            placeholder="Height (in)"
+            maxLength={10}
           />
-        </View>
 
-        <Text style={styles.label}>Height (in)</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="numeric"
-          onChangeText={setHeight}
-          value={height}
-          placeholder="Height (in)"
-          maxLength={10}
-        />
-
-        <Text style={styles.label}>Weight (lbs)</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="numeric"
-          onChangeText={setWeight}
-          value={weight}
-          placeholder="Weight (lbs)"
-          maxLength={10}
-        />
-
-        <Text style={styles.label}>Birth Date</Text>
-        <View style={styles.input}>
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={birthdate}
-            mode="date"
-            display="default"
-            onChange={(event, selectedDate) => {
-              const currentDate = selectedDate || birthdate;
-              setOpen(false);
-              setBirthdate(currentDate);
-            }}
+          <Text style={styles.label}>Weight (lbs)</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            onChangeText={setWeight}
+            value={weight}
+            placeholder="Weight (lbs)"
+            maxLength={10}
           />
+
+          <Text style={styles.label}>Birth Date</Text>
+          <View style={styles.input}>
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={birthdate}
+              mode="date"
+              display="default"
+              onChange={(event, selectedDate) => {
+                const currentDate = selectedDate || birthdate;
+                setOpen(false);
+                setBirthdate(currentDate);
+              }}
+            />
+          </View>
+
+          {/* ... add labels and input fields for frequency, gender, etc., similar to above ... */}
+
+          <Button title="Sign Up" onPress={handleSubmit} />
         </View>
-
-        {/* ... add labels and input fields for frequency, gender, etc., similar to above ... */}
-
-        <Button title="Sign Up" onPress={handleSubmit} />
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   );
 };
 
