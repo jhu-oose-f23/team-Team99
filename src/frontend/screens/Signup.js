fe46234c26c05de7a462876e0523eae2875f70cc
K02D <kirondeb02@gmail.com>
2023-12-06 11:15:04 -0500

    add dropdowns

--- src/frontend/screens/Signup.js
+++ src/frontend/screens/Signup.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { View, TextInput, Button, StyleSheet, Text } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-
+import DropDownPicker from "react-native-dropdown-picker";
 import UserContext from "../UserContext";
 import { postUser } from "../api";
 
@@ -13,11 +13,35 @@ const Signup = ({ navigation, route }) => {
   const [open, setOpen] = useState(false);
 
   const [frequency, setFrequency] = useState("");
+  const [frequencyOptions, setFrequencyOptions] = useState([
+    { label: "Daily", value: "Daily" },
+    { label: "3-4 times a week", value: "3-4 times a week" },
+    { label: "1 time a week", value: "1 time a week" },
+  ]);
+  const [frequencyDropdownOpen, setFrequencyDropdownOpen] = useState(false);
   const [gender, setGender] = useState("");
+  const [genderOptions, setGenderOptions] = useState([
+    { label: "Woman", value: "Woman" },
+    { label: "Man", value: "Man" },
+    { label: "Transgender", value: "Transgender" },
+    { label: "Non-binary/non-conforming", value: "Non-binary/non-conforming" },
+    { label: "Prefer not to respond", value: "Prefer not to respond" },
+  ]);
+  const [genderDropdownOpen, setGenderDropdownOpen] = useState(false);
+
+  const [preferredTime, setPreferredTime] = useState("");
+  const [preferredTimeOptions, setPreferredTimeOptions] = useState([
+    { label: "Morning", value: "Morning" },
+    { label: "Afternoon", value: "Afternoon" },
+    { label: "Evening", value: "Evening" },
+    { label: "Night", value: "Night" },
+  ]);
+  const [preferredTimeDropdownOpen, setPreferredTimeDropdownOpen] =
+    useState(false);
+
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
   const [goals, setGoals] = useState([]);
-  const [preferredTime, setPreferredTime] = useState("");
   const [level, setLevel] = useState("");
 
   const { setUserHasSignedUp } = useContext(UserContext);
@@ -57,6 +81,71 @@ const Signup = ({ navigation, route }) => {
         placeholder="Last Name"
         onChangeText={setLastName}
       />
+
+      <View style={{ zIndex: 1000, alignItems: "center" }}>
+        <Text style={{ marginRight: "50%" }}>Frequency</Text>
+        <DropDownPicker
+          open={frequencyDropdownOpen}
+          listMode="SCROLLVIEW"
+          value={frequency}
+          items={frequencyOptions}
+          setOpen={setFrequencyDropdownOpen}
+          setValue={(v) => setFrequency(v())}
+          placeholder="Frequency"
+          style={styles.dropdown}
+          placeholderStyle={{
+            color: "#C7C7CD",
+            marginBottom: 8,
+          }}
+          dropDownContainerStyle={{ ...styles.dropdown, zIndex: 1000 }}
+          containerStyle={{
+            maxHeight: 2000,
+          }}
+        />
+      </View>
+
+      <View style={{ zIndex: 100, alignItems: "center" }}>
+        <Text style={{ marginRight: "55%" }}>Gender</Text>
+        <DropDownPicker
+          open={genderDropdownOpen}
+          listMode="SCROLLVIEW"
+          value={gender}
+          items={genderOptions}
+          setOpen={setGenderDropdownOpen}
+          setValue={(v) => setGender(v())}
+          placeholder="Gender"
+          style={styles.dropdown}
+          placeholderStyle={{
+            color: "#C7C7CD",
+            marginBottom: 8,
+          }}
+          dropDownContainerStyle={styles.dropdown}
+          containerStyle={{
+            maxHeight: 2000,
+          }}
+        />
+      </View>
+      <View style={{ zIndex: 50, alignItems: "center" }}>
+        <Text style={{ marginRight: "45%" }}>Preferred Time</Text>
+        <DropDownPicker
+          open={preferredTimeDropdownOpen}
+          listMode="SCROLLVIEW"
+          value={preferredTime}
+          items={preferredTimeOptions}
+          setOpen={setPreferredTimeDropdownOpen}
+          setValue={(v) => setPreferredTime(v())}
+          placeholder="Preferred Time"
+          style={styles.dropdown}
+          placeholderStyle={{
+            color: "#C7C7CD",
+            marginBottom: 8,
+          }}
+          dropDownContainerStyle={styles.dropdown}
+          containerStyle={{
+            maxHeight: 2000,
+          }}
+        />
+      </View>
 
       <Text style={styles.label}>Height (in)</Text>
       <TextInput
@@ -106,6 +195,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  dropdown: {
+    width: "80%",
+    alignSelf: "center",
+    marginTop: 1,
+    margin: 5,
+  },
   input: {
     width: "80%", // Adjust width as needed
     height: 50, // Adjust height as needed
@@ -117,6 +212,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     alignContent: "center",
     alignItems: "center",
+    backgroundColor: "white",
   },
   row: {
     flexDirection: "row",
